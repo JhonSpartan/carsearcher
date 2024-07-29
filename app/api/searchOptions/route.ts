908803e11459c18838c4cd6bacb32e3b7aca895e
Pavel Kulinich <kulinich.pavel88@gmail.com>
2024-07-29 17:47:01 +0200

    execution mode added

--- app/api/searchOptions/route.ts
+++ app/api/searchOptions/route.ts
@@ -2,6 +2,7 @@ import SearchOptions from "@/libs/models/searchOptions.model";
 import { connectToDB } from "@/libs/mongoose";
 import { NextResponse } from "next/server";
 
+export const dynamic = 'force-dynamic';
 
 export async function GET() {
   await connectToDB();
